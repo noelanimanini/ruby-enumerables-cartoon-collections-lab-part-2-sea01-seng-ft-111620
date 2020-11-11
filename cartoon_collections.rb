217b16d7fffe526ed213996567c781bcf7453a79
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-11 03:52:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,10 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
+  def long_planeteer_calls(calls_long)
+  calls_long.any? do |i|
+    i.length > 4
+  end
 end
 
 def find_valid_calls(planeteer_calls)
